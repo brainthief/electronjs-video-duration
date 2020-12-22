e73e52c1a8dbfed46c955f48272633d932e37ba8
brainthief <vytautas1983@gmail.com>
2020-12-22 22:20:24 +0200

    window with debugger

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const electron = require("electron")
 
-const { app } = electron
+const { app, BrowserWindow, ipcMain } = electron
 
 app.on("ready", () => {
- console.log("App is now ready")
+ const mainWindows = new BrowserWindow({
+  webPreferences: {
+   nodeIntegration: true,
+  },
+ })
+ mainWindows.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on("video:submit", (event, path) => {
+ console.log(path)
 })
